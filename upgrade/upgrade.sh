f24ed05d226207b2ebb4c737155c3d258be8164a
rahul-subash <s.v.rahul1997@gmail.com>
2021-09-08 20:28:18 +0530

    Handled 4.2 upgrade

---
+++ upgrade/upgrade.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+# Copyright (c) Syncfusion Inc. All rights reserved.
+#
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --version=*)
+      version="${1#*=}"
+      ;;
+    --namespace=*)
+      namespace="${1#*=}"
+      ;;
+	--app_base_url=*)
+      app_base_url="${1#*=}"
+      ;;
+	--optional_libs=*)
+      optional_libs="${1#*=}"
+      ;;
+	--bing_map_enable=*)
+      bing_map_enable="${1#*=}"
+      ;;
+	--bing_map_api_key=*)
+      bing_map_api_key="${1#*=}"
+      ;;
+    *)
+  esac
+  shift
+done
+
+[ -n "$version" ] || read -p 'Enter the version to upgrade: ' version
+
+if [ -z "$bing_map_enable" ]; then bing_map_enable=""; fi
+if [ -z "$bing_map_api_key" ]; then bing_map_api_key=""; fi
+
+if [ -z "$version" ]
+then
+	echo "Version is empty."
+else	
+	if [ -z "$namespace" ]
+	then
+		namespace="default"
+	fi
+	
+	if [[ "$version" == *"4.2"* ]]
+	then
+		[ -n "$app_base_url" ] || read -p 'Enter the app_base_url: ' app_base_url
+		[ -n "$optional_libs" ] || read -p 'Enter the optional libraries needed to be installed [comma seperated]: ' optional_libs
+		
+		if [ ! -d "boldbi_4-2" ]; then mkdir boldbi_4-2; fi
+		
+		# Downloading deployment files.."
+		if [ ! -f "boldbi_4-2/log4net_config.yaml" ]; then curl -o boldbi_4-2/log4net_config.yaml https://raw.githubusercontent.com/boldbi/boldbi-kubernetes/v4.2_common_idp/upgrade/log4net_config.yaml; fi
+		if [ ! -f "boldbi_4-2/deployment.yaml" ]; then curl -o boldbi_4-2/deployment.yaml https://raw.githubusercontent.com/boldbi/boldbi-kubernetes/v4.2_common_idp/upgrade/deployment.yaml; fi
+		
+		# deployment file changes changes
+		sed -i "s/<namespace>/$namespace/g" boldbi_4-2/log4net_config.yaml
+		sed -i "s/<namespace>/$namespace/g" boldbi_4-2/deployment.yaml
+		sed -i "s/<image_tag>/$version/g" boldbi_4-2/deployment.yaml
+		sed -i "s,<application_base_url>,$app_base_url,g" boldbi_4-2/deployment.yaml
+		sed -i "s/<comma_separated_library_names>/$optional_libs/g" boldbi_4-2/deployment.yaml
+		sed -i "s/<widget_bing_map_enable>/$bing_map_enable/g" boldbi_4-2/deployment.yaml
+		sed -i "s/<widget_bing_map_api_key>/$bing_map_api_key/g" boldbi_4-2/deployment.yaml
+				
+		# applying new changes to cluster
+		kubectl apply -f boldbi_4-2/log4net_config.yaml
+		kubectl apply -f boldbi_4-2/deployment.yaml
+	fi
+fi
