ea9918fd77e7bcbf0b75e654a0017a770174f03a
rahul-subash <s.v.rahul1997@gmail.com>
2021-09-21 15:02:04 +0530

    Update 4-2-upgrade-patch.sh

--- upgrade/4-2-upgrade-patch.sh
+++ upgrade/4-2-upgrade-patch.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+utilitiesPath="/boldbi/utilities"
+
 apt-get install wget
 apt-get install unzip
 
 wget https://www.syncfusion.com/downloads/support/directtrac/general/ze/4-2-upgrader-patch1266244206.zip
 unzip 4-2-upgrader-patch1266244206.zip
 
+[ ! -d "$utilitiesPath" ] && mkdir -p "$utilitiesPath"
+cp -a 4-2-upgrader-patch/customwidgetupgrader "$utilitiesPath"
+
 dotnet 4-2-upgrader-patch/4-2-upgrade/4-2-upgrader.dll
 if [ -d "/boldbi/app_data/bi/dataservice/CustomWidgets" ]; then
-    dotnet 4-2-upgrader-patch/customwidgetupgrader/CustomWidgetUpgrader.dll true
+    cd "$utilitiesPath/customwidgetupgrader"
+    dotnet "CustomWidgetUpgrader.dll" true
 fi
